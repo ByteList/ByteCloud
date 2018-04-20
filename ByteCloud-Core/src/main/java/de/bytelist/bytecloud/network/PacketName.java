3939c41a6d8a38efc9b4a5ed974ffda08201a9d6
ByteList <niklas.bytelist@gmail.com>
2018-04-20 15:05:23 +0200

    Hotfix - changed get-class-name to hard-coded- string

--- ByteCloud-Core/src/main/java/de/bytelist/bytecloud/network/PacketName.java
+++ ByteCloud-Core/src/main/java/de/bytelist/bytecloud/network/PacketName.java
@@ -1,14 +1,5 @@
 package de.bytelist.bytecloud.network;
 
-import de.bytelist.bytecloud.network.bungee.PacketInBungee;
-import de.bytelist.bytecloud.network.bungee.PacketInBungeeStopped;
-import de.bytelist.bytecloud.network.bungee.PacketInStartServer;
-import de.bytelist.bytecloud.network.bungee.PacketInStopServer;
-import de.bytelist.bytecloud.network.cloud.*;
-import de.bytelist.bytecloud.network.server.PacketInChangeServerState;
-import de.bytelist.bytecloud.network.server.PacketInKickPlayer;
-import de.bytelist.bytecloud.network.server.PacketInServer;
-import de.bytelist.bytecloud.network.server.PacketInStopOwnServer;
 import lombok.Getter;
 
 /**
@@ -23,31 +14,31 @@ public enum PacketName {
     /**
         package: de.bytelist.bytecloud.network.bungee
      */
-    IN_BUNGEE(PacketType.BUNGEE, PacketInBungee.class.getSimpleName()),
-    IN_BUNGEE_STPOPPED(PacketType.BUNGEE, PacketInBungeeStopped.class.getSimpleName()),
-    IN_START_SERVER(PacketType.BUNGEE, PacketInStartServer.class.getSimpleName()),
-    IN_STOP_SERVER(PacketType.BUNGEE, PacketInStopServer.class.getSimpleName()),
+    IN_BUNGEE(PacketType.BUNGEE, "PacketInBungee"),
+    IN_BUNGEE_STPOPPED(PacketType.BUNGEE, "PacketInBungeeStopped"),
+    IN_START_SERVER(PacketType.BUNGEE, "PacketInStartServer"),
+    IN_STOP_SERVER(PacketType.BUNGEE, "PacketInStopServer"),
 
     /**
      package: de.bytelist.bytecloud.network.server
      */
-    IN_CHANGE_SERVER_STATE(PacketType.SERVER, PacketInChangeServerState.class.getName()),
-    IN_KICK_PLAYER(PacketType.SERVER, PacketInKickPlayer.class.getName()),
-    IN_SERVER(PacketType.SERVER, PacketInServer.class.getName()),
-    IN_STOP_OWN_SERVER(PacketType.SERVER, PacketInStopOwnServer.class.getName()),
+    IN_CHANGE_SERVER_STATE(PacketType.SERVER, "PacketInChangeServerState"),
+    IN_KICK_PLAYER(PacketType.SERVER, "PacketInKickPlayer"),
+    IN_SERVER(PacketType.SERVER, "PacketInServer"),
+    IN_STOP_OWN_SERVER(PacketType.SERVER, "PacketInStopOwnServer"),
 
     /**
      package: de.bytelist.bytecloud.network.cloud
      */
-    OUT_CHANGE_SERVER_STATE(PacketType.CLOUD, PacketOutChangeServerState.class.getName()),
-    OUT_CLOUD_INFO(PacketType.CLOUD, PacketOutCloudInfo.class.getName()),
-    OUT_EXECUTE_COMMAND(PacketType.CLOUD, PacketOutExecuteCommand.class.getName()),
-    @Deprecated OUT_KICK_ALL_PLAYERS(PacketType.CLOUD, PacketOutKickAllPlayers.class.getName()),
-    OUT_KICK_PLAYER(PacketType.CLOUD, PacketOutKickPlayer.class.getName()),
-    OUT_MOVE_PLAYER(PacketType.CLOUD, PacketOutMovePlayer.class.getName()),
-    OUT_REGISTER_PLAYER(PacketType.CLOUD, PacketOutRegisterServer.class.getName()),
-    OUT_SEND_MESSAGE(PacketType.CLOUD, PacketOutSendMessage.class.getName()),
-    OUT_UNREGISTER_SERVER(PacketType.CLOUD, PacketOutUnregisterServer.class.getName());
+    OUT_CHANGE_SERVER_STATE(PacketType.CLOUD, "PacketOutChangeServerState"),
+    OUT_CLOUD_INFO(PacketType.CLOUD, "PacketOutCloudInfo"),
+    OUT_EXECUTE_COMMAND(PacketType.CLOUD, "PacketOutExecuteCommand"),
+    @Deprecated OUT_KICK_ALL_PLAYERS(PacketType.CLOUD, "PacketOutKickAllPlayers"),
+    OUT_KICK_PLAYER(PacketType.CLOUD, "PacketOutKickPlayer"),
+    OUT_MOVE_PLAYER(PacketType.CLOUD, "PacketOutMovePlayer"),
+    OUT_REGISTER_PLAYER(PacketType.CLOUD, "PacketOutRegisterServer"),
+    OUT_SEND_MESSAGE(PacketType.CLOUD, "PacketOutSendMessage"),
+    OUT_UNREGISTER_SERVER(PacketType.CLOUD, "PacketOutUnregisterServer");
 
 
     @Getter
